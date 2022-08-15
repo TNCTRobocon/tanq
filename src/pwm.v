cb55b68ad4b1336cc8919160fc23570b0df28414
Teru Kawakami <teru@soum.co.jp>
2022-08-15 14:57:50 +0900

    pwm: change timimg to fetch period,compare

--- src/pwm.v
+++ src/pwm.v
@@ -1,22 +1,30 @@
 module pwm(
-        input				reset,clock,
-        input	   [31:0] 	peirod,
-        input 	   [31:0]   compare,
-        output     [31:0]  	count,
-        output 			    pulse
+        input				clock,reset,
+        input      [31:0]   pwm_peirod,pwm_compare,
+        output 			    pwm_pulse,
+        output              pwm_fetch
     );
 
-    reg [31:0] 	current;
-
-    always @(posedge clock)begin
+    reg [31:0] 	count,period,compare;
+    always @(posedge clock,posedge reset)begin
         if (reset)begin
-            current <=0;
+            count <=0;
+            period<=0;
+            compare<=0;
         end else begin
-            current <= (current+1 != peirod)?current+1:0;
+            if (count==period)begin
+                count<=0;
+                period<=pwm_peirod;
+                compare<=pwm_compare;
+            end else begin
+                count<=count+1;
+                period<=period;
+                compare<=compare;
+            end
         end
     end
 
-    assign count=current;
-    assign pulse= current<compare;
+    assign pwm_pulse=count<compare;
+    assign pwm_fetch=count==0;
 
 endmodule
--- src/pwm_test.v
+++ src/pwm_test.v
@@ -5,27 +5,28 @@ module pwm_test();
     reg        reset,clock;
     reg [31:0] peirod;
     reg [31:0] compare;
-    wire [31:0] count;
 
-    wire pulse;
+    wire pulse,fetch;
 
     pwm pwm0(
-            .reset(reset),.clock(clock),
-            .peirod(peirod),.compare(compare),
-            .count(count), .pulse(pulse));
+            .clock(clock),.reset(reset),
+            .pwm_peirod(peirod),.pwm_compare(compare),
+            .pwm_pulse(pulse),.pwm_fetch(fetch));
+
+    always #1 begin
+        clock<=!clock;
+    end
 
     initial begin
         $dumpfile("pwm_test.vcd");
         $dumpvars(0, pwm_test);
-        clock=0;
-        peirod=4;
+        clock<=0;reset<=1;
+        peirod<=4;
         compare=2;
-        reset=1; #1 reset=0;
+         #1 reset=0;
         #100 $finish;
     end
 
-    always #0.5 begin
-        clock=!clock;
-    end
+    
 
 endmodule
